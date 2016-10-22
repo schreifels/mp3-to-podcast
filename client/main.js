850110f4f40784efd12b53e5393f61e6d7027200
Mike Schreifels <mike@schreifels.org>
2016-10-22 00:29:07 -0700

    Toggle input element based on URL value

--- client/main.js
+++ client/main.js
@@ -3,19 +3,30 @@
 
   function Mp3ToPodcast() {
     this.onDomLoaded = this.onDomLoaded.bind(this);
+    this.toggleTitle = this.toggleTitle.bind(this);
     this.handleInput = this.handleInput.bind(this);
   }
 
   Mp3ToPodcast.prototype = {
     onDomLoaded: function() {
-      this.mp3UrlInputEl  = document.getElementsByClassName('mp3-url-input')[0];
+      this.mp3UrlInputEl = document.getElementsByClassName('mp3-url-input')[0];
       this.mp3TitleInputEl = document.getElementsByClassName('mp3-title-input')[0];
 
       this.mp3UrlInputEl.addEventListener('input', this.handleInput);
       this.mp3TitleInputEl.addEventListener('input', this.handleInput);
     },
 
+    toggleTitle: function(shouldShowTitle) {
+      if (shouldShowTitle) {
+        this.mp3TitleInputEl.parentElement.classList.remove('hidden');
+      } else {
+        this.mp3TitleInputEl.parentElement.classList.add('hidden');
+      }
+    },
+
     handleInput: function(e) {
+      this.toggleTitle(!!this.mp3UrlInputEl.value);
+
       // Putting mp3Url after title ensures that when the URL is pasted into iOS
       // Messages, the app links the full URL, even if title has special chars.
       const url = PODCAST_BASE_URL +
