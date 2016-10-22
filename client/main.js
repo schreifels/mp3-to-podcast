faea990a19409a852ec9ab49111f256e23d1cc95
Mike Schreifels <mike@schreifels.org>
2016-10-22 01:18:22 -0700

    Create podcast URL input and populate it

--- client/main.js
+++ client/main.js
@@ -23,33 +23,34 @@
 
   Mp3ToPodcast.prototype = {
     onDomLoaded: function() {
-      this.urlInputEl = document.getElementsByClassName('main-url-input')[0];
+      this.mp3UrlInputEl = document.getElementsByClassName('main-mp3-url-input')[0];
       this.titleInputEl = document.getElementsByClassName('main-title-input')[0];
       this.copyLabelColonEl = document.getElementsByClassName('main-copy-label-colon')[0];
+      this.podcastUrlInputEl = document.getElementsByClassName('main-podcast-url-input')[0];
 
       const throttledHandleChange = throttle(this.handleChange, 100);
-      this.urlInputEl.addEventListener('input', throttledHandleChange);
+      this.mp3UrlInputEl.addEventListener('input', throttledHandleChange);
       this.titleInputEl.addEventListener('input', throttledHandleChange);
 
       this.handleChange();
     },
 
     handleChange: function(e) {
-      if (this.urlInputEl.value || this.titleInputEl.value) {
+      if (this.mp3UrlInputEl.value || this.titleInputEl.value) {
         this.titleInputEl.parentElement.classList.remove('hidden');
         this.copyLabelColonEl.classList.remove('hidden');
       } else {
         this.titleInputEl.parentElement.classList.add('hidden');
         this.copyLabelColonEl.classList.add('hidden');
-        this.urlInputEl.focus();
+        this.mp3UrlInputEl.focus();
       }
 
       // Putting mp3Url after title ensures that when the URL is pasted into iOS
       // Messages, the app links the full URL, even if title has special chars.
-      const url = PODCAST_BASE_URL +
-                '?title=' + encodeURIComponent(this.titleInputEl.value) +
-                '&mp3Url=' + encodeURIComponent(this.urlInputEl.value);
-      console.log(url);
+      const podcastUrl = PODCAST_BASE_URL +
+                          '?title=' + encodeURIComponent(this.titleInputEl.value) +
+                          '&mp3Url=' + encodeURIComponent(this.mp3UrlInputEl.value);
+      this.podcastUrlInputEl.value = podcastUrl;
     }
   };
 
